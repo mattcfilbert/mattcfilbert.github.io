25d72e307964eea65f5bd7f750226be054f6e7da
MATT FILBERT <mcfilbert@email.wm.edu>
2017-08-30 17:31:32 -0400

    kinda working!

--- js/script.js
+++ js/script.js
@@ -1,7 +1,44 @@
 /* global $ */
+$(document).ready(() => {
+  $('#projects').on('click', () => {
+    $('#proj1').removeClass('hidden')
+    $('#goHome').removeClass('hidden')
+    $('#home').addClass('hidden')
+    $('#proj2').addClass('hidden')
+    $('#proj3').addClass('hidden')
+    $('#projects').addClass('hidden')
+    $('#show').removeClass('hidden')
+  })
 
-function playPsycho () {
-  document.getElementById('psycho').play()
-}
+  $('#goHome').on('click', () => {
+    $('#home').removeClass('hidden')
+    $('#goHome').addClass('hidden')
+    $('#proj1').addClass('hidden')
+    $('#proj2').addClass('hidden')
+    $('#proj3').addClass('hidden')
+    $('#projects').removeClass('hidden')
+    $('#show').addClass('hidden')
+  })
 
-$('.watermark').on('click', playPsycho)
+  $('#one').on('click', () => {
+    $('#proj1').removeClass('hidden')
+    $('#proj2').addClass('hidden')
+    $('#proj3').addClass('hidden')
+  })
+  $('#two').on('click', () => {
+    $('#proj1').addClass('hidden')
+    $('#proj2').removeClass('hidden')
+    $('#proj3').addClass('hidden')
+  })
+  $('#three').on('click', () => {
+    $('#proj1').addClass('hidden')
+    $('#proj2').addClass('hidden')
+    $('#proj3').removeClass('hidden')
+  })
+
+  function playPsycho () {
+    document.getElementById('psycho').play()
+  }
+
+  $('.watermark').on('click', playPsycho)
+})
